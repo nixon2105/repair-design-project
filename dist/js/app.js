1575bbf6fbbc942817e19aff16e26466a690e0d6
nixon2105 <radivilov1991@mail.ru>
2019-10-16 10:55:52 +0300

    feat: add function smooth scrooll

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,26 @@
 /* Menu nav toggle */
-$("#nav_toggle").on("click", function (event) {
-    event.preventDefault();
+$("#nav_toggle").on("click", function(event) {
+  event.preventDefault();
 
-    $(this).toggleClass("active");
-    $("#nav").toggleClass("active");
+  $(this).toggleClass("active");
+  $("#nav").toggleClass("active");
+});
+
+/* Smooth scroll */
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+
+  let $this = $(this),
+    blockId = $(this).data("scroll"),
+    blockOffset = $(blockId).offset().top;
+
+  $("#nav a").removeClass("active");
+  $this.addClass("active");
+
+  $("html, body").animate(
+    {
+      scrollTop: blockOffset
+    },
+    500
+  );
 });
